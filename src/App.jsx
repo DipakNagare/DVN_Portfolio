6dfb9ac82d3cd72ebef64e0873c367bbb1bd0e7e
DipakNagare <dipaknagare3526@gmail.com>
2024-01-14 03:43:55 +0530

    Adding laoder spinner before content page is load

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
-
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a 3-second loading delay
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 10000);
+
+    // Clear the timeout to prevent memory leaks
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div >
-        <div className='bg-About-pattern bg-cover bg-no-repeat bg-center'>
-        <About />
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <div className='bg-About-pattern bg-cover bg-no-repeat bg-center'>
+            <About />
+          </div>
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className='relative z-0'>
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
+      )}
     </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../LodingSpinner.css'
+const LoadingSpinner = () => {
+  return (
+    <div className="LoadingSpinner-wrapper">
+      <span className="LoadingSpinner"></span>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
